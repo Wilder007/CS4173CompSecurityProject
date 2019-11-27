2466c65d4fa8844b464b95f6d869b71fca28c7f7
Ryan Hernandez <ryanhernandez@ou.edu>
2019-11-27 11:56:49 -0600

    Added to Solution
    Added Transmission MVC solution to project. Added CarKeyAuthenticationService to encapsulate logic for authenticating keys. Cleaned up code to improve logic flow.

--- Keyless_Entry_Authentication/Keyless_Entry_Authentication/Interfaces/IKeylessEntryAuthentication.cs
+++ Keyless_Entry_Authentication/Keyless_Entry_Authentication/Interfaces/IKeylessEntryAuthentication.cs
@@ -8,13 +8,5 @@ namespace Keyless_Entry_Authentication.Interfaces
         bool Authenticate(byte[] transmission);
 
         bool TwoFactorAuthenticate(int id, byte[] transmission);
-
-        bool CompareKeys(CarInfo car, int keyId);
-
-        void UpdateKeyInfo(int keyId, int timesCalled, int timeSucc);
-
-        void AuthenticateKeyFob(int carId, int keyId);
-
-        int GenerateRandomKey();
     }
 }
---
+++ Keyless_Entry_Authentication/Keyless_Entry_Transmission/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Keyless_Entry_Transmission.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
